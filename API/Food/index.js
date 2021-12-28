79f1744968b90c94d0e0ec2dfd9eef16f648cc61
Sanskruti3082 <sans.ullagaddi3082@gmail.com>
2021-12-28 20:08:13 +0530

    Food API

---
+++ API/Food/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+
+import {FoodModel} from "../../database/allModels";
+
+const RRouter = express.Router();
+
+/*
+Route                /
+Des                 Get all the foods based on particular restaurants
+params               _id
+access              public
+method              get
+*/
+
+Router.get("/:_id", async(req,res)=> {
+  try {
+      const {_id} = req.params;
+      const foods= await FoodModel.find({restaurant:_id});
+      return res.json({foods});
+  } catch(error) {
+    return res.status(500).json({error: error.message});
+  }
+});
+
+/*
+Route                /r
+Des                 Get all the foods based on particular category
+params              category
+access              public
+method              get
+*/
+
+Router.get("/r/:category", async(res,req)=> {
+  try {
+    const {category} = req.params;
+    const foods = await FoodModel.find({
+      category: {$regex: category, $options: "i"}
+    });
+    return res.json({foods});
+  } catch (error) {
+    return res.status(500).json({error: error.message});
+  }
+});
+
+export default Router;
